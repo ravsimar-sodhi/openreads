1da694a8041de8af7690127432bf695e53a1e369
manojit32 <manojit270696@gmail.com>
2020-04-13 20:52:16 +0530

    new commit

--- book_recommender/Books/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Books.models import Book, Genre
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Genre)
-
--- book_recommender/UserGroups/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 7003b5a659b47ff10a98f9fa5808c8f11b231424
