2f9acc30b57de5965f43fd5e5cb991d4c88f74a1
Shubham Das <96bubun19@gmail.com>
2020-04-14 10:49:54 +0530

    Update forms.py

--- book_recommender/Users/forms.py
+++ book_recommender/Users/forms.py
@@ -2,15 +2,16 @@ from django import forms
 # from Users.models import User
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.core.exceptions import ValidationError
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
+    username = forms.CharField(label='Enter Username', min_length=4, max_length=150,required=True)
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=100, help_text='First Name')
-    last_name = forms.CharField(max_length=100, help_text='Last Name')
-    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+    first_name = forms.CharField(max_length=100, help_text='First Name',required=True)
+    last_name = forms.CharField(max_length=100, help_text='Last Name',required=True)
+    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput,required=True)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput,required=True)
 
     class Meta:
         model = User
@@ -26,6 +27,29 @@ class RegistrationForm(forms.Form):
 
 
         )
+        
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError("Username already exists")
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError("Email already exists")
+
+    def clean(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 != password2:
+            print("yo")
+            raise forms.ValidationError("password and confirm password does not match")
 
 
     def save(self, commit=True):
