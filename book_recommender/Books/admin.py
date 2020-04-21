01def89a9ce340c43f151660c6266f77c9f9a8de
Ravsimar Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2020-04-21 19:53:50 +0530

    slight change in details page

--- book_recommender/Books/admin.py
+++ book_recommender/Books/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from Books.models import Book, Genre
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Genre)
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'book_title', 'book_author', 'book_avg_rating', 'book_num_of_ratings']
+
+admin.site.register(Book, BookAdmin)
+
+admin.site.register(Genre)
